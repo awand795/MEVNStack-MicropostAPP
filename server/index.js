b574ae25e692b4c7025e446a4ddf156843952a22
awand795 <awand795@gmail.com>
2020-03-24 13:40:11 +0700

    Added static folder and prop spa

--- server/index.js
+++ server/index.js
@@ -13,6 +13,13 @@ app.use(cors());
 
 app.use('/api/posts',posts);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname+'/public'));
+    app.get(/.*/,(req,res)=>{
+        res.sendFile(__dirname+'/public/index.html');
+    });
+}
+
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
 });
